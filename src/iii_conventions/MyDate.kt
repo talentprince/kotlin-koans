b549f751abb727839c1578e46e28d0668e3c858b
Chen Chen <talent_prince@163.com>
2017-02-06 17:31:39 +0800

    Finish test 26

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -10,10 +10,15 @@ enum class TimeInterval {
     YEAR
 }
 
-class DateRange(val start: MyDate, val endInclusive: MyDate)
+class DateRange(val start: MyDate, val endInclusive: MyDate) {
+}
 
 operator fun MyDate.compareTo(other: MyDate): Int = when {
     this.year != other.year -> this.year - other.year
     this.month != other.month -> this.month - other.month
     else -> this.dayOfMonth - other.dayOfMonth
 }
+
+operator fun DateRange.contains(date: MyDate): Boolean = date >= this.start && date <= this.endInclusive
+
+
