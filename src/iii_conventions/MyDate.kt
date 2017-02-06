63cb3999e7119d5876c9b883a72416aa3be03f8c
Chen Chen <talent_prince@163.com>
2017-02-06 17:37:35 +0800

    Finish test 27

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -2,7 +2,7 @@ package iii_conventions
 
 data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
 
-operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
+operator fun MyDate.rangeTo(other: MyDate): DateRange =  DateRange(this, other)
 
 enum class TimeInterval {
     DAY,
