ae3402ab623c27fd90047f5ffb8c24155d49df9b
Chen Chen <talent_prince@163.com>
2017-02-07 17:10:38 +0800

    Finish test 31

--- src/iii_conventions/_31_Invoke_.kt
+++ src/iii_conventions/_31_Invoke_.kt
@@ -3,7 +3,15 @@ package iii_conventions
 import util.TODO
 
 
-class Invokable
+class Invokable {
+    var invokeTimes : Int = 0
+    operator fun invoke(): Invokable {
+        invokeTimes++
+        return this
+    }
+
+    fun getNumberOfInvocations(): Int = invokeTimes
+}
 
 fun todoTask31(): Nothing = TODO(
     """
@@ -14,6 +22,5 @@ fun todoTask31(): Nothing = TODO(
     references = { invokable: Invokable -> })
 
 fun task31(invokable: Invokable): Int {
-    todoTask31()
-//    return invokable()()()().getNumberOfInvocations()
+    return invokable()()()().getNumberOfInvocations()
 }
